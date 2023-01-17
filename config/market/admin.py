2a9c4bef15f23526216500d16f46d3f7690a600e
ANTANTTechnology <antant.technology@gmail.com>
2023-01-17 21:10:10 +0300

    Admin panel V1 + add categories to the Catalog.

--- config/market/admin.py
+++ config/market/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from .models import *
+
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ("title",)}
+
+class TagAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ("title",)}
+
+class LevelofDifficultyAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ("title",)}
+
+class ProductAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ("title",)}
+    list_display = ('id','title','price','is_published',)
+    list_display_links = ('title',)
+    search_fields = ('title','product_id')
 
 # Register your models here.
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Tag, TagAdmin)
+admin.site.register(LevelofDifficulty, LevelofDifficultyAdmin)
+admin.site.register(Product, ProductAdmin)
