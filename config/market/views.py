7ef9da26b5c69c37c8f79110821c22359befa451
ANTANTTechnology <antant.technology@gmail.com>
2023-01-19 01:56:03 +0300

    The first product and its display on the main page

--- config/market/views.py
+++ config/market/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import *
 
 # Create your views here.
 
 def index(request):
-    return render(request,template_name='market/base.html')
+    product = Product.objects.all()
+    category = Category.objects.all()
+
+    context = {
+        'product': product,
+        'category': category,
+    }
+    return render(request, template_name='market/base.html', context=context)
